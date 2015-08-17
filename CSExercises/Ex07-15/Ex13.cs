d9f962796d12a496acb9e1b0da39f710cfcc9041
hengcheewen <e0002161@u.nus.edu>
2015-08-18 04:43:29 +0800

    e0002161 Ex13

--- CSExercises/Ex07-15/Ex13.cs
+++ CSExercises/Ex07-15/Ex13.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ISS.RV.LIB;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,18 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            double distance, tfare;
             
+            Console.WriteLine("enter distance traveled by ABC cab =");
+            distance = Convert.ToDouble(Console.ReadLine());
+            tfare = 2.4 + distance * .4;
+            tfare = Math.Ceiling(tfare);
+            
+            Console.Write("round fare is {0:c}", tfare);
+            Console.ReadLine();
+            
+
+
         }
     }
 }
