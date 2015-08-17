7c739c52c2a8a433efd1726b4857adb9328a3c25
hengcheewen <e0002161@u.nus.edu>
2015-08-18 03:55:25 +0800

    e0002161 Ex10

--- CSExercises/Ex07-15/Ex10.cs
+++ CSExercises/Ex07-15/Ex10.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ISS.RV.LIB;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,27 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            //double X1, X2, Y1, Y2, D;
+            Console.WriteLine("Enter the coordinates of two points in the sequence of X1, Y1, X2, Y2");
+            Console.WriteLine("Compute the distance between two points using the formula of: ");
+            Console.WriteLine(" (X2-X1)^2 + (Y2-Y1)^2");
             
+
+            Console.Write("ENTER X2 = ");
+            double X2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ENTER X1 = ");
+            double X1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ENTER Y2 = ");
+            double Y2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ENTER Y1 = ");
+            double Y1 = Convert.ToDouble(Console.ReadLine());
+            double D = Math.Sqrt((X2 - X1)*(X2 - X1))+((Y2 - Y1)*(Y2-Y1));
+            Console.WriteLine("therefore the distance between two points =" + D);
+            
+            Console.ReadLine();
+
+
+
         }
     }
 }
