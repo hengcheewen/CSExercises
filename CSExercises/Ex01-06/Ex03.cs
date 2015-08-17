332e942c402f23c25886feb2d315151bf4a2708f
hengcheewen <e0002161@u.nus.edu>
2015-08-17 14:47:28 +0800

    e0002161 Ex03

--- CSExercises/Ex01-06/Ex03.cs
+++ CSExercises/Ex01-06/Ex03.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ISS.RV.LIB;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,15 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            double A;
+            double B;
+
+            Console.Write("Enter adouble precision number= ");
+            A = Convert.ToDouble(Console.ReadLine());
+            B = (A)*(A);
+            Console.WriteLine("Square of the precision number= " + B);
+            Console.ReadLine();
+
         }
     }
 }
