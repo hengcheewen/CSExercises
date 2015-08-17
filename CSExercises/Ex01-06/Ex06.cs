ef0187be43099eb3346a8cff78fc6048d53bb181
hengcheewen <e0002161@u.nus.edu>
2015-08-17 15:58:21 +0800

    e0002161 Ex06

--- CSExercises/Ex01-06/Ex06.cs
+++ CSExercises/Ex01-06/Ex06.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ISS.RV.LIB;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,18 @@ using System.Threading.Tasks;
 
 namespace CSExercises
 {
-    public class Ex06
+    class Ex06
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            
+            int A;
+            double B;
+
+            Console.Write("Enter an integer = ");
+            A = Convert.ToInt32(Console.ReadLine()); //not possible ToInt64
+            B = Math.Sqrt(A);
+            Console.WriteLine("Square root of integer= " + B);
+            Console.ReadLine();
         }
     }
 }
