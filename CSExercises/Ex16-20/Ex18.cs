0dacaa87eacc20774360936f029f35495397e69b
hengcheewen <e0002161@u.nus.edu>
2015-08-18 09:16:53 +0800

    e0002161 Ex18

--- CSExercises/Ex16-20/Ex18.cs
+++ CSExercises/Ex16-20/Ex18.cs
@@ -1,3 +1,11 @@
+
+using System;
+using ISS.RV.LIB;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex18
@@ -5,6 +13,27 @@ namespace CSExercises
         public static void Main(string[] args)
         {
             
+            Console.WriteLine(" Stident Mark=    ");
+
+            int marks=Convert.ToInt16(Console.ReadLine());
+            char lettergrade = ' ';
+            string error = "**Error**";
+
+            if ((marks >= 80) && (marks <= 100))
+                    lettergrade= 'A' ;
+            else if ((marks >= 60) && (marks <= 79))
+                    lettergrade= 'B';
+            else if ((marks >= 40) && (marks <= 59))
+                     lettergrade = 'C';
+            else if ((marks >= 0) && (marks <= 40))
+                     lettergrade = 'F';
+
+            if (lettergrade == ' ')
+                Console.WriteLine(error);
+            else
+                Console.WriteLine(" You scored {0} marks which is a {1} grade.", marks, lettergrade);
+           
+
         }
     }
 }
