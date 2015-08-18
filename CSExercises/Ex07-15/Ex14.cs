d94cb80ce3b7ed264f622ac8076ea2129d6466c9
hengcheewen <e0002161@u.nus.edu>
2015-08-18 08:45:12 +0800

    e0002161 Ex19

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISS.RV.LIB;
+
+
+
+
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
+            double a;
+            double b;
+            double c;
+
+            Console.WriteLine("enter length of 3 sides of a triangle ");
+
+            do
+            {
+                Console.WriteLine("a?");
+                string xa = Console.ReadLine();
+                a = Convert.ToDouble(xa);
+            } while (a <= 0);
+
+            do
+            {
+                Console.WriteLine("b?");
+                string xb = Console.ReadLine();
+                b = Convert.ToDouble(xb);
+            } while (b <= 0);
+
+            do
+            {
+                Console.WriteLine("c?");
+                string xc = Console.ReadLine();
+                c = Convert.ToDouble(xc);
+            } while (c <= 0);
+
             
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            
+            Console.Write("the total area of the triangle is");
+            Console.Write(area);
+            
+            System.Console.ReadLine();
+
+            
+
+
         }
     }
 }
--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -1,10 +1,26 @@
+using System;
+using ISS.RV.LIB;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
 namespace CSExercises
 {
     public class Ex19
     {
         public static void Main(string[] args)
         {
+            double tfare=0;
+            Console.WriteLine("enter distance travelled on ABC taxi =");
+            double distance = Convert.ToDouble(Console.ReadLine());
             
+            if (distance <= 0.5) tfare = 2.4;
+            if (distance >0.5) if(distance<=8.5) tfare=2.4+(0.4*(distance/0.1)-0.5);
+            if (distance>9) tfare=2.4+(0.4*((distance-0.5)/0.1))+(0.5*((distance-9)/0.1));
+            Console.WriteLine("therefore the total fare would be {0:c}", tfare);
         }
     }
 }
